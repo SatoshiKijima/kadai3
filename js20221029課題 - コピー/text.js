3fb3a02a2d0fcca388e244dfc68955660544c8f3
T59_キジ_W <116822970+SatoshiKijima@users.noreply.github.com>
2022-11-04 12:39:33 +0900

    Add files via upload

---
+++ js20221029課題 - コピー/text.js
@@ -0,0 +1,105 @@
+// 一番最初にalertでJavascriptの接続を確認する
+// alert(1111);
+console.log("はじめてのジャバスクリプト");
+console.log(23+5);
+console.log(2000-1800);
+console.log("18+5");
+
+// 変数は箱である 3つ変数はある
+// var name="就職キャンプ太郎"
+// let 
+// const 
+// var oohori ="年収1500万円のチーフおおほりさん";
+// console.log (oohori);
+
+// var yellow = "黄色";
+// console.log(yellow);
+
+// var name = "もりた";
+// console.log(name);
+
+// var hako=1;
+// if(hako===1) {console.log("1がはいっています");
+// } else if(hako===2) {console.log("2がはいっています");
+// } else if(hako===3) {console.log("3がはいっています");
+// }
+
+// var point=79;
+// if(point>=80) {
+//     console.log("素晴らしい！おめでとう‼");
+// } else {console.log("もっと頑張りましょう");
+// }
+
+// var point=80;
+// if(point>=80) {
+//     console.log("素晴らしい！おめでとう‼");
+// } else {console.log("もっと頑張りましょう");
+// }
+
+// var point=45;
+// if(point>80) {console.log("素晴らしい！おめでとう‼");
+// } else if(point<55) {console.log("マジかんべん");
+// } else if(point<30) {console.log("ごめんなさい");
+// }
+var ramdom= Math.floor(Math.random()*5+1);
+console.log(ramdom,"ランダムな数字")
+
+if(ramdom===0){
+    console.log("大吉");
+} else if(ramdom===2){
+    console.log("中吉");
+} else if(ramdom===3){
+    console.log("小吉");
+} else if(ramdom===4){
+    console.log("吉");
+} else if(ramdom===5){
+    console.log("凶");
+} 
+// 
+var random = Math.floor(Math.random() * 5);
+console.log(random, "ランダムな数字");
+
+if (random === 0) {
+  console.log("大吉");
+} else if (random === 1) {
+  console.log("中吉");
+} else if (random === 2) {
+  console.log("小吉");
+} else if (random === 3) {
+  console.log("吉");
+} else if (random === 4) {
+  console.log("末");
+}
+
+$(".a").on("click", function(){
+// 
+// alert("h1が押されました！");
+// // 
+// $(".a").css("color", "red");
+// // 
+// $(".a").fadeout(2000);
+// $(".a").fadein(2000);
+
+if (random === 0) {
+    console.log("大吉");
+    $(".a").text("大吉");
+    $(".a").css("color","red");
+    $(".a").css("text-aline,center");
+  } else if (random === 1) {
+    console.log("中吉");
+    $(".a").text("中吉");
+  } else if (random === 2) {
+    console.log("小吉");
+    $(".a").text("小吉");
+  } else if (random === 3) {
+    console.log("吉");
+    $(".a").text("吉");
+  } else if (random === 4) {
+    console.log("末");
+    $(".a").text("末");
+      }
+  
+});
+// ＄＋（）＋.を忘れず click, 
+// });削除しないで
+// fadeoutから書かないとfadeinが動かない
